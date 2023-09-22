4ad44a8416bd0121a137bc4d61eb56ba4b84cf71
Rishop Babu <rishopbabu22@gmail.com>
2023-09-22 13:22:37 +0530

    minor changes in stocklist tsx

--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -5,14 +5,27 @@ import {
   View,
   Text,
   FlatList,
-  ScrollView,
-  TouchableOpacity,
   StyleSheet,
+  Button,
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../store/configureStore'; // Import your store types
 import {getStocksData} from '../actions/stockActions';
 import {Stock} from '../types/stockTypes';
+
+function renderFlatList(item: Stock) {
+  return (
+  <View style={styles.textContainer}>
+        <Text style={styles.textItem}> symbol: {item.symbol}</Text>
+        <Text style={styles.textItem}> name: {item.name}</Text>
+        <Text style={styles.textItem}> currency: {item.currency}</Text>
+        <Text style={styles.textItem}> exchange: {item.exchange}</Text>
+        <Text style={styles.textItem}> mic_code: {item.mic_code}</Text>
+        <Text style={styles.textItem}> country: {item.country}</Text>
+        <Text style={styles.textItem}> type: {item.type}</Text>
+      </View>
+  )
+}
 
 const StockList: React.FC = () => {
   const dispatch = useDispatch();
@@ -33,73 +46,55 @@ const StockList: React.FC = () => {
   }
 
   return (
-    <>
     <View style={styles.container}>
-      <View style={styles.mainViewContainer}>
-        <Text style={styles.titleText}>Stocks List</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Stocks List!!</Text>
       </View>
       <FlatList
+        showsVerticalScrollIndicator={false}
+        style={styles.flatListContainer}
         data={stocks}
         keyExtractor={(item: Stock) => item.symbol}
-        renderItem={({item}) => (
-          <View>
-            <Text> symbol: {item.symbol}</Text>
-            <Text> name: {item.name}</Text>
-            <Text> currency: {item.currency}</Text>
-            <Text> exchange: {item.exchange}</Text>
-            <Text> mic_code: {item.mic_code}</Text>
-            <Text> country: {item.country}</Text>
-            <Text> type: {item.type}</Text>
-            <Text>
-              {' '}
-              -----------------------------------------------------------------------------
-            </Text>
-            <Text> $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$</Text>
-            <Text>
-              {' '}
-              -----------------------------------------------------------------------------
-            </Text>
-          </View>
-        )}
+        renderItem={item => renderFlatList(item.item)}
       />
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonTitle}>next page</Text>
-      </TouchableOpacity>
-      </View>
-    </>
+      <Button title="Next Page" onPress={() => {}}/>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'coolBackgroundColor', // Replace with your desired background color
+    backgroundColor: 'white',
   },
-  mainViewContainer: {
-    flex: 1,
-    justifyContent: 'center',
+  titleContainer: {
     alignContent: 'center',
-    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   titleText: {
     fontSize: 24,
     fontWeight: 'bold',
+    alignContent: 'center',
+  },
+  flatListContainer: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  textContainer: {
+    backgroundColor: 'lightgray',
+    marginVertical: 2,
+    padding: 5,
+  },
+  textItem: {
+    fontSize: 16,
   },
   button: {
-    backgroundColor: '#007AFF',
-    borderRadius: 5,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
     alignItems: 'center',
     marginBottom: 5,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
-  buttonTitle: {
-    color: '#FFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+    marginLeft: 20,
+    marginRight: 20,
+  }
 });
 
 export default StockList;
